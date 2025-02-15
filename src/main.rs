43f5d1ec8265315f964b946eb34c1b9f63d1ffb5
Gerixmus <gerixmuss@gmail.com>
2025-02-15 22:04:37 +0100

    feat: multi select demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use inquire::MultiSelect;
+
 fn main() {
-    println!("Hello, world!");
+    let options = vec![
+        "Option 1",
+        "Option 2",
+        "Option 3"
+    ];
+
+    let selected_options = MultiSelect::new("Select your options", options).prompt();
+
+    match selected_options {
+        Ok(choices) => {
+            if choices.is_empty() {
+                println!("No options selected.");
+            } else {
+                println!("You selected:");
+                for choice in choices {
+                    println!("- {}", choice);
+                }
+            }
+        }
+        Err(err) => println!("An error occurred: {}", err),
+    }   
 }
