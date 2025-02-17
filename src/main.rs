29f511f14414db6007250d4a9e26f406cc29e45b
Gerixmus <gerixmuss@gmail.com>
2025-02-17 23:13:22 +0100

    feat: added subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use clap::{Parser, Subcommand};
+
 mod git_operations;
 mod standard;
 
+#[derive(Parser)]
+#[command(name = "cmt", version = "1.0", about = "Commit management tool")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Ignore,
+}
+
 fn main() {
-    if let Err(err) = standard::run_standard() {
-        eprintln!("❌ Error: {}", err);
-        std::process::exit(1);
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Ignore) => {
+            println!("Ignore logic to implement later");
+        }
+        None => {
+            if let Err(err) = standard::run_standard() {
+                eprintln!("❌ Error: {}", err);
+                std::process::exit(1);
+            }
+        }
     }
 }
