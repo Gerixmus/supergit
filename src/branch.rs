df424725e21cb0590433ecc82631904e499bda62
Gerixmus <gerixmuss@gmail.com>
2025-02-24 22:28:57 +0100

    feat: logic for checkout and branch list

--- src/branch.rs
+++ src/branch.rs
@@ -3,7 +3,9 @@ use crate::git_operations;
 pub fn run_branch() -> Result<(), String> {
     let branches = git_operations::get_branches()?;
 
-    branches.iter().for_each(|branch| {println!("{}", branch)});
+    for branch in &branches {
+        println!("{}", branch);
+    }
 
     Ok(())
 }
