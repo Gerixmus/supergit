10a888ffd1c48c6d41b4763e0dca43723ef2ac0f
Gerixmus <gerixmuss@gmail.com>
2025-08-11 15:47:08 +0200

    feat: add push after commit option

--- src/init.rs
+++ src/init.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
+    pub push_commits: bool,
     pub conventional_commits: bool,
     pub conventional_branches: bool,
     pub ticket_prefix: bool
@@ -14,6 +15,7 @@ pub struct Config {
 impl Config {
     pub fn default() -> Self {
         Config {
+            push_commits: false,
             conventional_commits: false,
             conventional_branches: false,
             ticket_prefix: false
@@ -32,10 +34,12 @@ fn get_config_path() -> PathBuf {
 pub fn load_config() -> Config {
     let config_path = get_config_path();
 
+    println!("{}", config_path.display());
+
     let config_content = fs::read_to_string(&config_path)
         .unwrap_or_else(|_| toml::to_string(&Config::default()).unwrap());
 
-    toml::from_str(&config_content).expect("Failed to parse config")
+    toml::from_str(&config_content).expect("Failed to parse config") 
 }
 
 pub fn run_config() -> Result<(), String> {
@@ -58,6 +62,7 @@ fn save_config(config: &Config, config_path: &PathBuf) -> io::Result<()> {
 
 fn create_config() -> Config {
     let settings = vec![
+        "push_commits",
         "conventional_commits",
         "conventional_branches",
         "ticket_prefix"
@@ -68,6 +73,7 @@ fn create_config() -> Config {
         .unwrap_or(vec![]);
 
     Config {
+        push_commits: selected_options.contains(&"push_commits"),
         conventional_commits: selected_options.contains(&"conventional_commits"),
         conventional_branches: selected_options.contains(&"conventional_branches"),
         ticket_prefix: selected_options.contains(&"ticket_prefix")
